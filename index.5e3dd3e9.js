3df89770abbb868a673f373fa4dd6b14a9b4aa0a
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-08-16 18:05:32 +0000

    Deploying to gh-pages from @ Volodymyr-Zastavniuk/team-project-icecream@90f4a0e933d36353c1f237980450f2d5d4993d81 🚀

---
+++ index.5e3dd3e9.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-hero-text-btn"),t=document.querySelector("[data-hero-text]");e.addEventListener("click",(()=>{t.classList.toggle("hero-text-is-open")}))})();
+//# sourceMappingURL=index.5e3dd3e9.js.map
---
+++ index.d0e3d2c8.js
@@ -0,0 +1,2 @@
+!function(){var e,t;e=document.querySelector("[data-hero-text-btn"),t=document.querySelector("[data-hero-text]"),e.addEventListener("click",(function(){t.classList.toggle("hero-text-is-open")}))}();
+//# sourceMappingURL=index.d0e3d2c8.js.map
