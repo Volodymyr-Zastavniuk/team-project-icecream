57c13456c23b4ff847ab006f701e2c8b18ef8a08
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-08-17 00:00:13 +0300

    adds slider to reviews

---
+++ src/slider-start.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 400,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+  });
+});
